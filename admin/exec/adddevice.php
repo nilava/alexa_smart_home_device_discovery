6b276d68d69e1de95200b630da130ed4ef4c62b0
nilava <nilavachowdhury99@gmail.com>
2019-03-05 13:53:01 +0530

    add device fix

--- admin/exec/adddevice.php
+++ admin/exec/adddevice.php
@@ -11,15 +11,27 @@ $bkey = $_POST['bkey'];
 $bsupport = $_POST['bsupport'];
 $csupport = $_POST['csupport'];
 $ret = $_POST['retrievable'];
-
-$sql = "INSERT INTO " .$_POST['room']. " (friendlyName, description, device_category, Auth_Token, Switch_Virtual_Key, brightness_virtual_key, brightness_support, color_support, retrievable)
- VALUES ('$name', '$description', '$category', '$token', '$switchkey', '$bkey', '$bsupport', '$csupport', '$ret');";
+$endpointid = generateRandomString();
+$sql = "INSERT INTO " .$_POST['room']. " (endpointId, friendlyName, description, device_category, Auth_Token, Switch_Virtual_Key, brightness_virtual_key, brightness_support, color_support, retrievable)
+ VALUES ('$endpointid', '$name', '$description', '$category', '$token', '$switchkey', '$bkey', '$bsupport', '$csupport', '$ret');";
 
  if ($conn->multi_query($sql) === TRUE) {
-    echo "New records created successfully";
+    echo "New Device Added successfully";
 } else {
     echo "Error: <br>" . $conn->error;
 }
-
 }
+
+
+
+function generateRandomString($length = 8) {
+    $characters = '0123456789';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
 ?>
