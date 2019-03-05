bc12c9a0b43b491c0c78947e814c892ffe163fe3
Nilava Chowdhury <nilavachowdhury99@gmail.com>
2019-03-05 22:30:36 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,14 +1,15 @@
 <?php
  error_reporting( ~E_DEPRECATED & ~E_NOTICE );
  
- define('DBHOST', '$DB_HOST');
+ define('DBHOST', $DB_HOST);
  define('DBUSER', '$DB_USER');
  define('DBPASS', '$DB_PASS');
  define('DBNAME', '$DB_NAME');
  
  $conn = mysqli_connect(DBHOST,DBUSER,DBPASS);
  $dbcon = mysqli_select_db($conn,DBNAME);
- 
+ echo DBHOST;
+echo DBUSER;
  if ( !$conn ) {
   die("Connection failed : " . mysqli_error());
  }
