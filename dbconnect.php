57b0ebaf85c82700ffede03ec8b2b06fb56f3f25
nilava <nilavachowdhury99@gmail.com>
2019-03-15 16:15:00 +0530

    dbconnect

---
+++ dbconnect.php
@@ -0,0 +1,17 @@
+<?php
+ error_reporting( ~E_DEPRECATED & ~E_NOTICE );
+ 
+$DBHOST = getenv("DB_HOST");
+$DBUSER = getenv("DB_USER");
+$DBPASS = getenv("DB_PASS");
+$DBNAME = getenv("DB_NAME");
+ $conn = mysqli_connect($DBHOST,$DBUSER,$DBPASS);
+ $dbcon = mysqli_select_db($conn,$DBNAME);
+ if ( !$conn ) {
+  die("Connection failed : " . mysqli_error());
+ }
+ 
+ if ( !$dbcon ) {
+  die("Database Connection failed : " . mysqli_error());
+ }
+?>
