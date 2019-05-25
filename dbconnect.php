0667bd57c58328778aa78834bce528775ec9cd4b
Nilava Chowdhury <nilavachowdhury99@gmail.com>
2019-05-25 23:36:29 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -6,7 +6,7 @@ $DBPORT = getenv("DB_PORT");
 $DBUSER = getenv("DB_USER");
 $DBPASS = getenv("DB_PASS");
 $DBNAME = getenv("DB_NAME");
- $conn = mysqli_connect($DBHOST,$DBUSER,$DBPASS,$DBPORT);
+ $conn = mysqli_connect($DBHOST,$DBUSER,$DBPASS,$DBPORT,MYSQL_CLIENT_SSL);
  $dbcon = mysqli_select_db($conn,$DBNAME);
  if ( !$conn ) {
   die("Connection failed : " . mysqli_error());
